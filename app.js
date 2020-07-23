81de6ee94ee1d625be5c74076f35370172aa83fa
John Rucker <JHRucker@outlook.com>
2020-07-23 10:40:12 -0500

    .

--- app.js
+++ app.js
@@ -25,143 +25,177 @@ var sense = null;
 
 console.log('Decrypting encryption key using AWS Master Key....')
 var keyMan = new KeyManger(['JensTitsef1c55a2-1808-450c-824a-62556d46b7b5'], '/opt/rGauge/certs/awsCredentials.json', __dirname + '/cmk.json');
+setupKeyManEventConsumers()
 
-keyMan.on('Error', ((errTxt, err)=>{
-    console.log('Encryption error: ' + errTxt)
-    console.dir(err, {depth:null});
+function setupKeyManEventConsumers() {
+    keyMan.on('Error', ((errTxt, err) => {
+        console.log('Encryption error: ' + errTxt)
+        console.dir(err, { depth: null });
 
-    console.log('We should send the following to gdtMan so it can be logged and then restart if error is restarable...')
-    console.log('This is the text to send to gdtMan -> keyManger Error: ' + err.message);
-    if(err.retryable == true){
-        console.log('We may be able to recover from this error.  Retrying in ' + err.retryDelay);
-    };
-
-    console.log('retrying in ' + err.retryDelay)
-    setTimeout(()=>{
-        console.log('Bang we retried...');
-        keyMan = new KeyManger(['JensTitsef1c55a2-1808-450c-824a-62556d46b7b5'], '/opt/rGauge/certs/awsCredentials.json', __dirname + '/cmk.json');
-    },err.retryDelay * 1000);
-
-}));
-
-keyMan.on('keyIsReady', (keyObj) => {
-    console.log('Encryption key decrypted and ready for use. Getting encrypted configuation data...');
-    console.dir(keyObj, {depth:null});
-    var keys = Object.keys(keyObj);
-    const myAppMan = new MyAppMan(__dirname + '/gaugeConfig.json', __dirname + '/modifiedConfig.encrypted', true, keyObj[keys[0]]);
-    const gaugePwrDstrbtn = new irTransmitter(pwrDstrbtnGC.gaugeIrAddress, pwrDstrbtnGC.calibrationTable);
-    const gaugeSlrPwr = new irTransmitter(slrPwrGC.gaugeIrAddress, slrPwrGC.calibrationTable);
-    const gaugeRnwblPrcnt = new irTransmitter(rnwblPrcntGC.gaugeIrAddress, rnwblPrcntGC.calibrationTable);
-
-    myAppMan.on('Update', () => {
-        console.log('New update event has fired.  Reloading gauge objects...');
-        myAppMan.setGaugeStatus('Config updated received. Please wait, may take up to 5 minutes to reload gauge objects. ' + (new Date()).toLocaleTimeString() + ', ' + (new Date()).toLocaleDateString());
-        clearInterval(mainPoller);
-        console.log('Re-Init senseData with new config...');
-        sense = new SenseData(myAppMan.config.userID, myAppMan.config.userPW);
-        setupSenseEvents();
-    });
-
-    myAppMan.on('userPW', () => {
-        console.log('A new user PW event received.');
-        if (myAppMan.userID != 'notSet' && myAppMan.userPW != 'notSet') {
-            console.log('Received new user ID and Password.  Getting new keys...');
-            myAppMan.setGaugeStatus('Received new user ID and Password.');
-            var objToSave = {
-                userID: myAppMan.userID,
-                userPW: myAppMan.userPW
-            };
-            myAppMan.saveItem(objToSave);
-        } else {
-            console.log('Login ID and Password must both be set.  Enter login ID first then password. Try agian in that order.');
-            myAppMan.setGaugeStatus('Login ID and Password must both be set.  Enter login ID first then password. Try agian in that order.');
+        console.log('We should send the following to gdtMan so it can be logged and then restart if error is restarable...')
+        console.log('This is the text to send to gdtMan -> keyManger Error: ' + err.message);
+        if (err.retryable == true) {
+            console.log('We may be able to recover from this error.  Retrying in ' + err.retryDelay);
         };
-        myAppMan.teslaUserID = 'notSet';
-        myAppMan.teslaUserPW = 'notSet';
-    });
 
-    console.log('First data call will occur in ' + (randomStart / 1000).toFixed(2) + ' seconds.');
-    console.log('When a Sense connection is established a poller will open and close a web socket every 1 minute, read trend data every ' + getTrendInterval + ' minutes, and re-authenticate every ' + reconnectInterval + ' minutes.');
+        console.log('retrying in ' + err.retryDelay)
+        setTimeout(() => {
+            console.log('Retrying to setup keyManager object...');
+            keyMan = new KeyManger(['JensTitsef1c55a2-1808-450c-824a-62556d46b7b5'], '/opt/rGauge/certs/awsCredentials.json', __dirname + '/cmk.json');
+            setupKeyManEventConsumers();
+        }, err.retryDelay * 1000);
 
-    function startPoller() {
-        console.log('Starting endless poller.');
-        clearInterval(mainPoller);
-        mainPoller = setInterval(() => {
-            minCount++;
-            if (nextGetTrendInterval == minCount) {
-                nextGetTrendInterval += reconnectInterval;
-                getTrend();
-            } else if (nextReconnectInterval == minCount) {
-                nextReconnectInterval += getTrendInterval;
-                reconnectToSense();
+    }));
+
+    keyMan.on('keyIsReady', (keyObj) => {
+        console.log('Encryption key decrypted and ready for use. Getting encrypted configuation data...');
+        console.dir(keyObj, { depth: null });
+        var keys = Object.keys(keyObj);
+        const myAppMan = new MyAppMan(__dirname + '/gaugeConfig.json', __dirname + '/modifiedConfig.encrypted', true, keyObj[keys[0]]);
+        const gaugePwrDstrbtn = new irTransmitter(pwrDstrbtnGC.gaugeIrAddress, pwrDstrbtnGC.calibrationTable);
+        const gaugeSlrPwr = new irTransmitter(slrPwrGC.gaugeIrAddress, slrPwrGC.calibrationTable);
+        const gaugeRnwblPrcnt = new irTransmitter(rnwblPrcntGC.gaugeIrAddress, rnwblPrcntGC.calibrationTable);
+
+        myAppMan.on('Update', () => {
+            console.log('New update event has fired.  Reloading gauge objects...');
+            myAppMan.setGaugeStatus('Config updated received. Please wait, may take up to 5 minutes to reload gauge objects. ' + (new Date()).toLocaleTimeString() + ', ' + (new Date()).toLocaleDateString());
+            clearInterval(mainPoller);
+            console.log('Re-Init senseData with new config...');
+            sense = new SenseData(myAppMan.config.userID, myAppMan.config.userPW);
+            setupSenseEvents();
+        });
+
+        myAppMan.on('userPW', () => {
+            console.log('A new user PW event received.');
+            if (myAppMan.userID != 'notSet' && myAppMan.userPW != 'notSet') {
+                console.log('Received new user ID and Password.  Getting new keys...');
+                myAppMan.setGaugeStatus('Received new user ID and Password.');
+                var objToSave = {
+                    userID: myAppMan.userID,
+                    userPW: myAppMan.userPW
+                };
+                myAppMan.saveItem(objToSave);
             } else {
-                sense.openWebSocket();
-            }
-        }, 60000);
-    };
+                console.log('Login ID and Password must both be set.  Enter login ID first then password. Try agian in that order.');
+                myAppMan.setGaugeStatus('Login ID and Password must both be set.  Enter login ID first then password. Try agian in that order.');
+            };
+            myAppMan.teslaUserID = 'notSet';
+            myAppMan.teslaUserPW = 'notSet';
+        });
 
-    function reconnectToSense() {
-        console.log('-------- Reconnecting to sense.com ---------');
-        sense.closeWebSoc();
-        sense.authenticate();
-        sense.openWebSocket();
-    };
+        console.log('First data call will occur in ' + (randomStart / 1000).toFixed(2) + ' seconds.');
+        console.log('When a Sense connection is established a poller will open and close a web socket every 1 minute, read trend data every ' + getTrendInterval + ' minutes, and re-authenticate every ' + reconnectInterval + ' minutes.');
 
-    function getTrend() {
-        sense.getTrends('week')
-            .then((data) => {
-                solarPowered = data.solar_powered
-                console.log('Update. This week ' + solarPowered + '% of the power was from renewable energy.');
-            })
-            .catch((err) => {
-                console.error('Error trying to getTrends from sense.com', err)
-            });
-    };
+        function startPoller() {
+            console.log('Starting endless poller.');
+            clearInterval(mainPoller);
+            mainPoller = setInterval(() => {
+                minCount++;
+                if (nextGetTrendInterval == minCount) {
+                    nextGetTrendInterval += reconnectInterval;
+                    getTrend();
+                } else if (nextReconnectInterval == minCount) {
+                    nextReconnectInterval += getTrendInterval;
+                    reconnectToSense();
+                } else {
+                    sense.openWebSocket();
+                }
+            }, 60000);
+        };
 
-    function setupSenseEvents() {
-        sense.on('authenticated', () => {
-            console.log('GDT Authenticated with sense!')
-            startPoller();
-            console.log('Getting trend data...');
+        function reconnectToSense() {
+            console.log('-------- Reconnecting to sense.com ---------');
+            sense.closeWebSoc();
+            sense.authenticate();
+            sense.openWebSocket();
+        };
+
+        function getTrend() {
             sense.getTrends('week')
                 .then((data) => {
                     solarPowered = data.solar_powered
-                    console.log('This week ' + solarPowered + '% of the power was from renewable energy.');
-                    if (firstRun) {
-                        sense.openWebSocket();
-                        firstRun = false;
-                    }
+                    console.log('Update. This week ' + solarPowered + '% of the power was from renewable energy.');
                 })
                 .catch((err) => {
-                    console.error('Error getTrends after authenticated', err);
-                })
-        });
+                    console.error('Error trying to getTrends from sense.com', err)
+                });
+        };
 
-        sense.on('notAuthenticated', () => {
-            console.log('Please check your sense login ID and Password.');
-            clearInterval(mainPoller);
-            myAppMan.setGaugeStatus('Error not authenticated with Sense Home Monitor at ' + (new Date()).toLocaleTimeString() + ', ' + (new Date()).toLocaleDateString());
-            if (inAlert == false) {
-                myAppMan.sendAlert({ [myAppMan.config.descripition]: "1" });
-                inAlert = true;
-            };
-        });
+        function setupSenseEvents() {
+            sense.on('authenticated', () => {
+                console.log('GDT Authenticated with sense!')
+                startPoller();
+                console.log('Getting trend data...');
+                sense.getTrends('week')
+                    .then((data) => {
+                        solarPowered = data.solar_powered
+                        console.log('This week ' + solarPowered + '% of the power was from renewable energy.');
+                        if (firstRun) {
+                            sense.openWebSocket();
+                            firstRun = false;
+                        }
+                    })
+                    .catch((err) => {
+                        console.error('Error getTrends after authenticated', err);
+                    })
+            });
 
-        sense.on('power', () => {
-            console.log((new Date()).toLocaleTimeString() +
-                ' | Home Load:' + sense.power.netWatts +
-                ', Solar In: ' + sense.power.solarWatts +
-                ', Grid In: ' + sense.power.gridWatts * -1 +
-                ' | ' + solarPowered + '% of the this week\'s power was from renewable energy.'
-            );
+            sense.on('notAuthenticated', () => {
+                console.log('Please check your sense login ID and Password.');
+                clearInterval(mainPoller);
+                myAppMan.setGaugeStatus('Error not authenticated with Sense Home Monitor at ' + (new Date()).toLocaleTimeString() + ', ' + (new Date()).toLocaleDateString());
+                if (inAlert == false) {
+                    myAppMan.sendAlert({ [myAppMan.config.descripition]: "1" });
+                    inAlert = true;
+                };
+            });
 
-            if (netWatts.length < 4) {
-                if (netWatts.length == 1 && netWatts[0] == -99999) {
-                    console.log('Reporting first run values...');
-                    netWatts = [];
-                    myAppMan.setGaugeValue(sense.power.netWatts, ' watts, ' +
-                        sense.power.solarWatts + " solar, " +
-                        sense.power.gridWatts * -1 + " grid, " +
+            sense.on('power', () => {
+                console.log((new Date()).toLocaleTimeString() +
+                    ' | Home Load:' + sense.power.netWatts +
+                    ', Solar In: ' + sense.power.solarWatts +
+                    ', Grid In: ' + sense.power.gridWatts * -1 +
+                    ' | ' + solarPowered + '% of the this week\'s power was from renewable energy.'
+                );
+
+                if (netWatts.length < 4) {
+                    if (netWatts.length == 1 && netWatts[0] == -99999) {
+                        console.log('Reporting first run values...');
+                        netWatts = [];
+                        myAppMan.setGaugeValue(sense.power.netWatts, ' watts, ' +
+                            sense.power.solarWatts + " solar, " +
+                            sense.power.gridWatts * -1 + " grid, " +
+                            solarPowered + " solar%, " +
+                            (new Date()).toLocaleTimeString()
+                        );
+                        myAppMan.setGaugeStatus('Okay, ' + (new Date()).toLocaleTimeString() + ', ' + (new Date()).toLocaleDateString());
+                        if (inAlert == true) {
+                            myAppMan.sendAlert({ [myAppMan.config.descripition]: "0" });
+                            inAlert = false;
+                        };
+                        gaugeRnwblPrcnt.sendValue(solarPowered);
+                        gaugeSlrPwr.sendValue(sense.power.solarWatts);
+                        gaugePwrDstrbtn.sendValue(sense.power.gridWatts * -1);
+
+                    };
+                    netWatts.push(sense.power.netWatts);
+                    solarWatts.push(sense.power.solarWatts);
+                    gridWatts.push(sense.power.gridWatts);
+                } else {
+                    var avgNetWatts = netWatts.reduce((p, c) => p + c, 0) / netWatts.length;
+                    var avgSolarWatts = solarWatts.reduce((p, c) => p + c, 0) / solarWatts.length;
+                    var avgGridWatts = gridWatts.reduce((p, c) => p + c, 0) / gridWatts.length;
+
+                    console.log((new Date()).toLocaleTimeString() +
+                        ' 5 Min Avg | Home Load:' + avgNetWatts +
+                        ', Solar In: ' + avgSolarWatts +
+                        ', Grid In: ' + avgGridWatts * -1 +
+                        ' | ' + solarPowered + '% of the this week\'s power was from renewable energy.'
+                    );
+
+                    myAppMan.setGaugeValue(avgNetWatts, ' watts, ' +
+                        avgSolarWatts + " solar, " +
+                        avgGridWatts * -1 + " grid, " +
                         solarPowered + " solar%, " +
                         (new Date()).toLocaleTimeString()
                     );
@@ -171,56 +205,27 @@ keyMan.on('keyIsReady', (keyObj) => {
                         inAlert = false;
                     };
                     gaugeRnwblPrcnt.sendValue(solarPowered);
-                    gaugeSlrPwr.sendValue(sense.power.solarWatts);
-                    gaugePwrDstrbtn.sendValue(sense.power.gridWatts * -1);
+                    gaugeSlrPwr.sendValue(avgSolarWatts);
+                    gaugePwrDstrbtn.sendValue(avgGridWatts * -1);
 
+                    netWatts = [];
+                    solarWatts = [];
+                    gridWatts = [];
                 };
-                netWatts.push(sense.power.netWatts);
-                solarWatts.push(sense.power.solarWatts);
-                gridWatts.push(sense.power.gridWatts);
-            } else {
-                var avgNetWatts = netWatts.reduce((p, c) => p + c, 0) / netWatts.length;
-                var avgSolarWatts = solarWatts.reduce((p, c) => p + c, 0) / solarWatts.length;
-                var avgGridWatts = gridWatts.reduce((p, c) => p + c, 0) / gridWatts.length;
 
-                console.log((new Date()).toLocaleTimeString() +
-                    ' 5 Min Avg | Home Load:' + avgNetWatts +
-                    ', Solar In: ' + avgSolarWatts +
-                    ', Grid In: ' + avgGridWatts * -1 +
-                    ' | ' + solarPowered + '% of the this week\'s power was from renewable energy.'
-                );
-
-                myAppMan.setGaugeValue(avgNetWatts, ' watts, ' +
-                    avgSolarWatts + " solar, " +
-                    avgGridWatts * -1 + " grid, " +
-                    solarPowered + " solar%, " +
-                    (new Date()).toLocaleTimeString()
-                );
-                myAppMan.setGaugeStatus('Okay, ' + (new Date()).toLocaleTimeString() + ', ' + (new Date()).toLocaleDateString());
-                if (inAlert == true) {
-                    myAppMan.sendAlert({ [myAppMan.config.descripition]: "0" });
-                    inAlert = false;
-                };
-                gaugeRnwblPrcnt.sendValue(solarPowered);
-                gaugeSlrPwr.sendValue(avgSolarWatts);
-                gaugePwrDstrbtn.sendValue(avgGridWatts * -1);
-
-                netWatts = [];
-                solarWatts = [];
-                gridWatts = [];
-            };
-
-            sense.closeWebSoc();
+                sense.closeWebSoc();
 
 
-        });
-    };
+            });
+        };
 
-    setTimeout(() => {
-        sense = new SenseData(myAppMan.config.userID, myAppMan.config.userPW)
-        setupSenseEvents();
-    }, randomStart);
-});
+        setTimeout(() => {
+            sense = new SenseData(myAppMan.config.userID, myAppMan.config.userPW)
+            setupSenseEvents();
+        }, randomStart);
+    });
+}
+
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
